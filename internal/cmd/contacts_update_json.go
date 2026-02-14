0c2aabb7e9fe71c7f82231c0aee35fad19fac4c9
Peter Steinberger <steipete@gmail.com>
2026-02-14 15:22:26 +0100

    refactor(contacts): reduce gocyclo for list clears

--- internal/cmd/contacts_update_json.go
+++ internal/cmd/contacts_update_json.go
@@ -68,6 +68,153 @@ func appendUnique(ss []string, v string) []string {
 	return append(ss, v)
 }
 
+var contactsPersonListForceSend = map[string]func(*people.Person) bool{
+	"addresses": func(p *people.Person) bool {
+		if p.Addresses == nil {
+			p.Addresses = []*people.Address{}
+		}
+		return len(p.Addresses) == 0
+	},
+	"biographies": func(p *people.Person) bool {
+		if p.Biographies == nil {
+			p.Biographies = []*people.Biography{}
+		}
+		return len(p.Biographies) == 0
+	},
+	"birthdays": func(p *people.Person) bool {
+		if p.Birthdays == nil {
+			p.Birthdays = []*people.Birthday{}
+		}
+		return len(p.Birthdays) == 0
+	},
+	"calendarUrls": func(p *people.Person) bool {
+		if p.CalendarUrls == nil {
+			p.CalendarUrls = []*people.CalendarUrl{}
+		}
+		return len(p.CalendarUrls) == 0
+	},
+	"clientData": func(p *people.Person) bool {
+		if p.ClientData == nil {
+			p.ClientData = []*people.ClientData{}
+		}
+		return len(p.ClientData) == 0
+	},
+	"emailAddresses": func(p *people.Person) bool {
+		if p.EmailAddresses == nil {
+			p.EmailAddresses = []*people.EmailAddress{}
+		}
+		return len(p.EmailAddresses) == 0
+	},
+	"events": func(p *people.Person) bool {
+		if p.Events == nil {
+			p.Events = []*people.Event{}
+		}
+		return len(p.Events) == 0
+	},
+	"externalIds": func(p *people.Person) bool {
+		if p.ExternalIds == nil {
+			p.ExternalIds = []*people.ExternalId{}
+		}
+		return len(p.ExternalIds) == 0
+	},
+	"genders": func(p *people.Person) bool {
+		if p.Genders == nil {
+			p.Genders = []*people.Gender{}
+		}
+		return len(p.Genders) == 0
+	},
+	"imClients": func(p *people.Person) bool {
+		if p.ImClients == nil {
+			p.ImClients = []*people.ImClient{}
+		}
+		return len(p.ImClients) == 0
+	},
+	"interests": func(p *people.Person) bool {
+		if p.Interests == nil {
+			p.Interests = []*people.Interest{}
+		}
+		return len(p.Interests) == 0
+	},
+	"locales": func(p *people.Person) bool {
+		if p.Locales == nil {
+			p.Locales = []*people.Locale{}
+		}
+		return len(p.Locales) == 0
+	},
+	"locations": func(p *people.Person) bool {
+		if p.Locations == nil {
+			p.Locations = []*people.Location{}
+		}
+		return len(p.Locations) == 0
+	},
+	"memberships": func(p *people.Person) bool {
+		if p.Memberships == nil {
+			p.Memberships = []*people.Membership{}
+		}
+		return len(p.Memberships) == 0
+	},
+	"miscKeywords": func(p *people.Person) bool {
+		if p.MiscKeywords == nil {
+			p.MiscKeywords = []*people.MiscKeyword{}
+		}
+		return len(p.MiscKeywords) == 0
+	},
+	"names": func(p *people.Person) bool {
+		if p.Names == nil {
+			p.Names = []*people.Name{}
+		}
+		return len(p.Names) == 0
+	},
+	"nicknames": func(p *people.Person) bool {
+		if p.Nicknames == nil {
+			p.Nicknames = []*people.Nickname{}
+		}
+		return len(p.Nicknames) == 0
+	},
+	"occupations": func(p *people.Person) bool {
+		if p.Occupations == nil {
+			p.Occupations = []*people.Occupation{}
+		}
+		return len(p.Occupations) == 0
+	},
+	"organizations": func(p *people.Person) bool {
+		if p.Organizations == nil {
+			p.Organizations = []*people.Organization{}
+		}
+		return len(p.Organizations) == 0
+	},
+	"phoneNumbers": func(p *people.Person) bool {
+		if p.PhoneNumbers == nil {
+			p.PhoneNumbers = []*people.PhoneNumber{}
+		}
+		return len(p.PhoneNumbers) == 0
+	},
+	"relations": func(p *people.Person) bool {
+		if p.Relations == nil {
+			p.Relations = []*people.Relation{}
+		}
+		return len(p.Relations) == 0
+	},
+	"sipAddresses": func(p *people.Person) bool {
+		if p.SipAddresses == nil {
+			p.SipAddresses = []*people.SipAddress{}
+		}
+		return len(p.SipAddresses) == 0
+	},
+	"urls": func(p *people.Person) bool {
+		if p.Urls == nil {
+			p.Urls = []*people.Url{}
+		}
+		return len(p.Urls) == 0
+	},
+	"userDefined": func(p *people.Person) bool {
+		if p.UserDefined == nil {
+			p.UserDefined = []*people.UserDefined{}
+		}
+		return len(p.UserDefined) == 0
+	},
+}
+
 func forceSendEmptyPersonListField(p *people.Person, personField string) {
 	if p == nil {
 		return
@@ -77,107 +224,11 @@ func forceSendEmptyPersonListField(p *people.Person, personField string) {
 		return
 	}
 
-	// Google API client omits empty values by default. For list clears, we need to:
-	// - ensure the field is an empty (non-nil) slice
-	// - add the Go struct field name to ForceSendFields
-	switch personField {
-	case "addresses":
-		if p.Addresses == nil {
-			p.Addresses = []*people.Address{}
-		}
-	case "biographies":
-		if p.Biographies == nil {
-			p.Biographies = []*people.Biography{}
-		}
-	case "birthdays":
-		if p.Birthdays == nil {
-			p.Birthdays = []*people.Birthday{}
-		}
-	case "calendarUrls":
-		if p.CalendarUrls == nil {
-			p.CalendarUrls = []*people.CalendarUrl{}
-		}
-	case "clientData":
-		if p.ClientData == nil {
-			p.ClientData = []*people.ClientData{}
-		}
-	case "emailAddresses":
-		if p.EmailAddresses == nil {
-			p.EmailAddresses = []*people.EmailAddress{}
-		}
-	case "events":
-		if p.Events == nil {
-			p.Events = []*people.Event{}
-		}
-	case "externalIds":
-		if p.ExternalIds == nil {
-			p.ExternalIds = []*people.ExternalId{}
-		}
-	case "genders":
-		if p.Genders == nil {
-			p.Genders = []*people.Gender{}
-		}
-	case "imClients":
-		if p.ImClients == nil {
-			p.ImClients = []*people.ImClient{}
-		}
-	case "interests":
-		if p.Interests == nil {
-			p.Interests = []*people.Interest{}
-		}
-	case "locales":
-		if p.Locales == nil {
-			p.Locales = []*people.Locale{}
-		}
-	case "locations":
-		if p.Locations == nil {
-			p.Locations = []*people.Location{}
-		}
-	case "memberships":
-		if p.Memberships == nil {
-			p.Memberships = []*people.Membership{}
-		}
-	case "miscKeywords":
-		if p.MiscKeywords == nil {
-			p.MiscKeywords = []*people.MiscKeyword{}
-		}
-	case "names":
-		if p.Names == nil {
-			p.Names = []*people.Name{}
-		}
-	case "nicknames":
-		if p.Nicknames == nil {
-			p.Nicknames = []*people.Nickname{}
-		}
-	case "occupations":
-		if p.Occupations == nil {
-			p.Occupations = []*people.Occupation{}
-		}
-	case "organizations":
-		if p.Organizations == nil {
-			p.Organizations = []*people.Organization{}
-		}
-	case "phoneNumbers":
-		if p.PhoneNumbers == nil {
-			p.PhoneNumbers = []*people.PhoneNumber{}
-		}
-	case "relations":
-		if p.Relations == nil {
-			p.Relations = []*people.Relation{}
-		}
-	case "sipAddresses":
-		if p.SipAddresses == nil {
-			p.SipAddresses = []*people.SipAddress{}
-		}
-	case "urls":
-		if p.Urls == nil {
-			p.Urls = []*people.Url{}
-		}
-	case "userDefined":
-		if p.UserDefined == nil {
-			p.UserDefined = []*people.UserDefined{}
-		}
-	default:
+	ensureFn := contactsPersonListForceSend[personField]
+	if ensureFn == nil {
+		return
+	}
+	if !ensureFn(p) {
 		return
 	}
 
